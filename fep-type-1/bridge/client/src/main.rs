eef693058f178f5ffcdc6f71fee5d9f2f3a1717a
Arnau <arnaubennassar5@gmail.com>
2024-10-01 04:28:20 +0200

    clean code

--- fep-type-1/bridge/client/src/main.rs
+++ fep-type-1/bridge/client/src/main.rs
@@ -11,7 +11,7 @@ use alloy_sol_macro::sol;
 
 sol! (
     function getLastInjectedGER() public view returns (bytes32);
-    function checkGERsAreConsecutiveAndReturnLastLER(bytes32 intitialGER, bytes32[] GERs) public view returns (bool, bytes32);
+    function checkGERsAreConsecutiveAndReturnLastLER(bytes32[] GERs) public view returns (bool, bytes32);
     function checkGERsExistance(bytes32[] calldata GERs) public view returns (bool);
 );
 
@@ -24,41 +24,64 @@ pub fn main() {
 
     // Verify bridge:
     // 1. Get the the last GER of the previous block on L2
-    assert_eq!(input.get_last_injected_ger_l2_prev_block_call.header.hash_slow(), input.prev_l2_block_hash);
-    let executor = ClientExecutor::new(input.get_last_injected_ger_l2_prev_block_call).unwrap();
-    let get_last_injected_ger_prev_block_input = ContractInput {
+    assert_eq!(
+        input.get_last_injected_ger_l2_prev_block_call.header.hash_slow(),
+        input.prev_l2_block_hash
+    );
+    let executor = ClientExecutor::new(
+        input.get_last_injected_ger_l2_prev_block_call
+    ).unwrap();
+    let get_last_injected_ger_l2_prev_block_input = ContractInput {
         contract_address: input.l2_ger_addr,
         caller_address: CALLER,
         calldata: getLastInjectedGERCall {},
     };
-    let get_injected_ger_index_prev_block_call_output = executor.execute(get_last_injected_ger_prev_block_input).unwrap();
+    let get_last_injected_ger_l2_prev_block_output = executor.execute(
+        get_last_injected_ger_l2_prev_block_input
+    ).unwrap();
     let initial_ger = getLastInjectedGERCall::abi_decode_returns(
-        &get_injected_ger_index_prev_block_call_output.contractOutput, 
-        false).unwrap()._0;
+        &get_last_injected_ger_l2_prev_block_output.contractOutput, true
+    ).unwrap()._0;
+    if !input.injected_gers.is_empty() {
+        assert_eq!(initial_ger, input.injected_gers[0]);
+    }
 
     // 2. Check that the GERs are consecutive on L2 at the new block
-    assert_eq!(input.check_gers_are_consecutive_and_return_last_ler_call_l2_new_block_call.header.hash_slow(), input.new_l2_block_hash);
-    let executor: ClientExecutor = ClientExecutor::new(input.check_gers_are_consecutive_and_return_last_ler_call_l2_new_block_call).unwrap();
-    let check_gers_consecutiveness_input = ContractInput {
+    assert_eq!(
+        input.check_gers_are_consecutive_and_return_last_ler_call_l2_new_block_call.header.hash_slow(),
+        input.new_l2_block_hash
+    );
+    let executor: ClientExecutor = ClientExecutor::new(
+        input.check_gers_are_consecutive_and_return_last_ler_call_l2_new_block_call
+    ).unwrap();
+    let check_gers_are_consecutive_and_return_last_ler_call_l2_new_block_input = ContractInput {
         contract_address: input.l2_ger_addr,
         caller_address: CALLER,
-        calldata: checkGERsAreConsecutiveAndReturnLastLERCall { intitialGER: initial_ger, GERs: input.injected_gers.clone()},
+        calldata: checkGERsAreConsecutiveAndReturnLastLERCall { GERs: input.injected_gers.clone()},
     };
-    let check_gers_consecutiveness_output = executor.execute(check_gers_consecutiveness_input).unwrap();
-    let call_result = checkGERsAreConsecutiveAndReturnLastLERCall::abi_decode_returns(&check_gers_consecutiveness_output.contractOutput, false).unwrap();
+    let check_gers_are_consecutive_and_return_last_ler_call_l2_new_block_output = executor.execute(
+        check_gers_are_consecutive_and_return_last_ler_call_l2_new_block_input
+    ).unwrap();
+    let call_result = checkGERsAreConsecutiveAndReturnLastLERCall::abi_decode_returns(
+        &check_gers_are_consecutive_and_return_last_ler_call_l2_new_block_output.contractOutput, true
+    ).unwrap();
     assert_eq!(call_result._0, true);
     assert_eq!(call_result._1, input.new_ler);
 
     // 3. Check that the GERs exist on L1
     assert_eq!(input.check_gers_existance_l1_call.header.hash_slow(), input.l1_block_hash);
-    let executor: ClientExecutor = ClientExecutor::new(input.check_gers_existance_l1_call).unwrap();
-    let check_gers_existance_input = ContractInput {
+    let executor: ClientExecutor = ClientExecutor::new(
+        input.check_gers_existance_l1_call
+    ).unwrap();
+    let check_gers_existance_l1_input = ContractInput {
         contract_address: input.l1_ger_addr,
         caller_address: CALLER,
         calldata: checkGERsExistanceCall { GERs: input.injected_gers.clone() },
     };
-    let check_gers_existance_call_output = executor.execute(check_gers_existance_input).unwrap();
-    let gers_exist = checkGERsExistanceCall::abi_decode_returns(&check_gers_existance_call_output.contractOutput, false).unwrap()._0;
+    let check_gers_existance_l1_output = executor.execute(check_gers_existance_l1_input).unwrap();
+    let gers_exist = checkGERsExistanceCall::abi_decode_returns(
+        &check_gers_existance_l1_output.contractOutput, true
+    ).unwrap()._0;
     assert_eq!(gers_exist, true);
 
     // Commit the bridge proof.

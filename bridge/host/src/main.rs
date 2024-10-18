1cc9c89c4834d9d4cf7eadb0b6a576f7d1dc12b7
Manav Darji <manavdarji.india@gmail.com>
2024-10-17 18:32:21 -0700

    fix warns

--- bridge/host/src/main.rs
+++ bridge/host/src/main.rs
@@ -94,7 +94,7 @@ async fn main() -> eyre::Result<()> {
         imported_gers = Vec::with_capacity(imported_gers_splitted.len());
         for ger_hex in imported_gers_splitted {
             imported_gers.push(alloy_primitives::FixedBytes::from_slice(
-                &hex::decode(&ger_hex).unwrap(),
+                &hex::decode(ger_hex).unwrap(),
             ));
         }
     }
@@ -160,10 +160,7 @@ async fn main() -> eyre::Result<()> {
     println!("Checking injectedGERs on L2 finished");
 
     // Check that the check was successful
-    assert_eq!(
-        check_injected_gers_and_return_ler_call_output_decoded.success,
-        true
-    );
+    assert!(check_injected_gers_and_return_ler_call_output_decoded.success);
 
     // Now that we've executed all of the calls, get the `EVMStateSketch` from the host executor.
     let executor_check_injected_gers_and_return_ler_sketch =
@@ -191,7 +188,7 @@ async fn main() -> eyre::Result<()> {
         .await?;
     println!("Checking injectedGERs on L1 finished");
     // Check that the check was successful
-    assert_eq!(check_injected_gers_existance_decoded.success, true);
+    assert!(check_injected_gers_existance_decoded.success);
 
     // Now that we've executed all of the calls, get the `EVMStateSketch` from the host executor.
     let executor_check_injected_gers_existance = executor_check_gers_existance.finalize().await?;
